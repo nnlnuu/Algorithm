2bae6b0b78051203219fd10c20d9a782b143dac2
문현우 <86148366+nnlnuu@users.noreply.github.com>
2023-06-27 19:12:58 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+/**
+ * [BRONZE 5] A + B -4
+ * @writer : nnlnuu
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(0),cin.tie(0)
+int main(void){
+    FASTIO;
+    int a,b;
+    while(1){
+        cin >> a >> b;
+        if(cin.eof()){
+            break;
+        }cout << a + b << "\n";
+    }
+}
