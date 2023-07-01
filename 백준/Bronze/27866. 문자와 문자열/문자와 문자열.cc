471452322aff1ea07ab97c1b1ad1894398e8fd31
문현우 <86148366+nnlnuu@users.noreply.github.com>
2023-07-01 15:48:56 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+/**
+ * [BRONZE 5] 문자와 문자열
+ * @writer : nnlnuu
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(0),cin.tie(0);
+int main(void){
+    string str;
+    int idx;
+    cin >> str;
+    cin >> idx;
+    cout << str[idx-1];
+
+    return 0;
+}
