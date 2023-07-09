0c79d0a998fb21f684581e27818270f459c0af72
문현우 <86148366+nnlnuu@users.noreply.github.com>
2023-07-09 17:14:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+/**
+ * [BRONZE 5] 아스키 코드
+ * @writer : nnlnuu
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(0),cin.tie(0);
+int main(void){
+    char c;
+    int i ;
+    cin >> c;
+    i = int(c);
+    cout << i;
+    return 0;
+
+}
