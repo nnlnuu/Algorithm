8a825bd7c31c99ec7112ca5a195cb93103aefe67
문현우 <86148366+nnlnuu@users.noreply.github.com>
2023-07-12 15:19:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+/**
+ * [Bronze 5] 새싹
+ * @writer : nnlnuu
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(0),cin.tie(0);
+int main(void){
+cout <<"         ,r'\"7\n";
+cout <<"r`-_   ,'  ,/ \n";
+cout <<" \\. \". L_r' \n";
+cout <<"   `~\\/ \n";
+cout <<"      |\n";
+cout <<"      |\n";
+
+    return 0;
+}
