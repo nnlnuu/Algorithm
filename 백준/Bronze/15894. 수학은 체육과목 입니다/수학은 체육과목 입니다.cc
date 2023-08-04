f38efbe34a4931174757ca67431cf6901e93b550
문현우 <86148366+nnlnuu@users.noreply.github.com>
2023-08-04 19:53:22 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+/**
+* [BRONZE 3] 수학은 체육과목입니다.
+* @writer : nnlnuu
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(0),cin.tie(0);
+
+int main(void){
+    FASTIO
+    long long n;
+    cin >> n;
+    cout << 4*n;
+    return 0;
+}
