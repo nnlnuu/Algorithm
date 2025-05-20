22972c0fd34a2a11323843a750c6c45c71f99209
문현우 <86148366+nnlnuu@users.noreply.github.com>
2025-05-20 12:18:40 +0900

    [Bronze II] Title: 단어의 개수, Time: 164 ms, Memory: 172104 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -0,0 +1,11 @@
+'''
+    B2 1152 | TODO Tier
+    알고리즘 분류 : TODO {1:분류명}
+    날짜 : 2025-05-20 12:16
+'''
+import sys
+input = sys.stdin.readline
+
+s = list(map(str,input().split()))
+
+print(len(s))
